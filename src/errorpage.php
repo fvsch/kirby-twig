f1456324c5591b568fd8fbab5b2e0923936644bc
Florens Verschelde <florens@fvsch.com>
2016-09-24 12:50:31 +0200

    Cosmetic: follow PSR-2 style

--- src/errorpage.php
+++ src/errorpage.php
@@ -1,120 +1,120 @@
 <!doctype html>
 <html lang="en">
 <head>
-	<meta charset="utf-8">
-	<meta name="robots" content="noindex">
-	<title><?php echo $title ?></title>
-	<style>
-	html {
-		font-family: sans-serif;
-		background-color: #eee;
-	}
-	body {
-		background-color: #fff;
-		color: #333;
-		margin: 0;
-		line-height: 1.5;
-	}
-	div {
-		padding: 1rem 1.5rem;
-	}
-	h1 {
-		margin: .5rem 0 1rem;
-		font-size: 140%;
-		line-height: 1.2;
-		font-family: monospace, monospace;
-	}
-	h2 {
-		margin: -.75rem 0 1rem;
-		font-size: 100%;
-		line-height: 1.3;
-		font-weight: normal;
-		font-family: monospace, monospace;
-		color: #707070;
-	}
-	p {
-		margin: .5rem 0;
-	}
-	pre, code {
-		font-family: monospace, monospace;
-	}
-	pre {
-		margin: 0;
-		padding: 1.5rem;
-		white-space: pre-wrap;
-		font-size: 90%;
-		-webkit-tab-size: 4;
-		-moz-tab-size: 4;
-		tab-size: 4;
-		color: #999;
-		background: black;
-	}
-	pre code {
-		display: block;
-	}
-	pre mark {
-		color: white;
-		background: none;
-	}
+    <meta charset="utf-8">
+    <meta name="robots" content="noindex">
+    <title><?php echo $title ?></title>
+    <style>
+    html {
+        font-family: sans-serif;
+        background-color: #eee;
+    }
+    body {
+        background-color: #fff;
+        color: #333;
+        margin: 0;
+        line-height: 1.5;
+    }
+    div {
+        padding: 1rem 1.5rem;
+    }
+    h1 {
+        margin: .5rem 0 1rem;
+        font-size: 140%;
+        line-height: 1.2;
+        font-family: monospace, monospace;
+    }
+    h2 {
+        margin: -.75rem 0 1rem;
+        font-size: 100%;
+        line-height: 1.3;
+        font-weight: normal;
+        font-family: monospace, monospace;
+        color: #707070;
+    }
+    p {
+        margin: .5rem 0;
+    }
+    pre, code {
+        font-family: monospace, monospace;
+    }
+    pre {
+        margin: 0;
+        padding: 1.5rem;
+        white-space: pre-wrap;
+        font-size: 90%;
+        -webkit-tab-size: 4;
+        -moz-tab-size: 4;
+        tab-size: 4;
+        color: #999;
+        background: black;
+    }
+    pre code {
+        display: block;
+    }
+    pre mark {
+        color: white;
+        background: none;
+    }
 
-	@media (min-width: 40em) {
-		html {
-			padding: 1em;
-		}
-		body {
-			width: 54em;
-			max-width: 100%;
-			margin: 1em auto;
-			border: solid 1px #ccc;
-		}
-	}
+    @media (min-width: 40em) {
+        html {
+            padding: 1em;
+        }
+        body {
+            width: 54em;
+            max-width: 100%;
+            margin: 1em auto;
+            border: solid 1px #ccc;
+        }
+    }
 
-	@media (max-width: 29.99em) {
-		div {
-			padding: 1.5rem 1rem;
-		}
-		pre {
-			padding: 1.5rem 1rem;
-			overflow-x: auto;
-		}
-		pre code {
-			padding-left: 0;
-		}
-	}
+    @media (max-width: 29.99em) {
+        div {
+            padding: 1.5rem 1rem;
+        }
+        pre {
+            padding: 1.5rem 1rem;
+            overflow-x: auto;
+        }
+        pre code {
+            padding-left: 0;
+        }
+    }
 
-	@media (min-width: 30em) {
-		[data-line] {
-			display: inline-block;
-			box-sizing: border-box;
-			width: 100%;
-			padding-left: 6ch;
-		}
-		[data-line]::before {
-			content: attr(data-line);
-			display: inline-block;
-			box-sizing: border-box;
-			width: 5ch;
-			margin-left: -7ch;
-			padding-right: 2ch;
-			text-align: right;
-			color: #666;
-			-webkit-user-select: none;
-			-moz-user-select: none;
-			-ms-user-select: none;
-			user-select: none;
-		}
-		mark[data-line]::before {
-			color: #bbb;
-		}
-	}
-	</style>
+    @media (min-width: 30em) {
+        [data-line] {
+            display: inline-block;
+            box-sizing: border-box;
+            width: 100%;
+            padding-left: 6ch;
+        }
+        [data-line]::before {
+            content: attr(data-line);
+            display: inline-block;
+            box-sizing: border-box;
+            width: 5ch;
+            margin-left: -7ch;
+            padding-right: 2ch;
+            text-align: right;
+            color: #666;
+            -webkit-user-select: none;
+            -moz-user-select: none;
+            -ms-user-select: none;
+            user-select: none;
+        }
+        mark[data-line]::before {
+            color: #bbb;
+        }
+    }
+    </style>
 </head>
 <body>
-	<div>
-		<h1><?php echo $title ?></h1>
-		<?php if (!empty($file)) { echo "<h2>$file</h2>\n"; } ?>
-		<?php if (!empty($message)) { echo "<p>$message</p>\n"; } ?>
-	</div>
-	<?php if (!empty($code)) { echo "<pre><code>$code</code></pre>"; } ?>
+    <div>
+        <h1><?php echo $title ?></h1>
+        <?php if (!empty($file)) { echo "<h2>$file</h2>\n"; } ?>
+        <?php if (!empty($message)) { echo "<p>$message</p>\n"; } ?>
+    </div>
+    <?php if (!empty($code)) { echo "<pre><code>$code</code></pre>"; } ?>
 </body>
 </html>
--- twig.php
+++ twig.php
@@ -2,19 +2,19 @@
 
 if (c::get('twig', false)) {
 
-	if (!class_exists('Kirby\Component\Template')) {
-		throw new Exception('Twig plugin requires Kirby 2.3 or higher. Current version: ' . kirby()->version());
-	}
-	if (!class_exists('Twig_Environment')) {
-		if (file_exists($loader = __DIR__.'/lib/Twig/Twig/Autoloader.php')) {
-			require_once $loader;
-			\Twig_Autoloader::register();
-		} else {
-			throw new Exception('Twig plugin: cannot find the Twig library');
-		}
-	}
+    if (!class_exists('Kirby\Component\Template')) {
+        throw new Exception('Twig plugin requires Kirby 2.3 or higher. Current version: ' . kirby()->version());
+    }
+    if (!class_exists('Twig_Environment')) {
+        if (file_exists($loader = __DIR__.'/lib/Twig/Twig/Autoloader.php')) {
+            require_once $loader;
+            \Twig_Autoloader::register();
+        } else {
+            throw new Exception('Twig plugin: cannot find the Twig library');
+        }
+    }
 
-	require_once __DIR__ . '/src/component.php';
-	kirby()->set('component', 'template', 'Kirby\Plugin\Twig\TwigComponent');
+    require_once __DIR__ . '/src/component.php';
+    kirby()->set('component', 'template', 'Kirby\Plugin\Twig\TwigComponent');
 
 }
